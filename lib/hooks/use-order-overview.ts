4725e64220dd3c8fd13f8e6ce85e51233d42b3e0
AFei <laterfei@qq.com>
2023-10-16 23:26:55 +0800

    fix:order-overview

--- lib/hooks/use-order-overview.ts
+++ lib/hooks/use-order-overview.ts
@@ -30,6 +30,7 @@ export function useOrderOverview() {
   const { expiration } = usePoolFormat(pool);
 
   const handleOrderOverviewDisplay = () => {
+    if (!quoteAmount || !baseAmount || !quotePrice) return;
     const size = quoteAmount ? formatNum(quoteAmount, 8, true) : null;
     const margin = baseAmount ? formatNum(baseAmount) : null;
 
