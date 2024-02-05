dd6f64c93ad71b67029cf5d41ba3695167f3937b
fei guo <812773673@qq.com>
2024-02-05 19:58:17 +0800

    feat:wallet-icon

--- components/share/wallet-select-dialog.tsx
+++ components/share/wallet-select-dialog.tsx
@@ -1,5 +1,6 @@
 import { atom, useAtom } from "jotai";
 import DialogGimp from "./dialog-gimp";
+import Image from "next/image";
 
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { useWallet } from "@solana/wallet-adapter-react";
@@ -25,10 +26,19 @@ export default function WalletSelectDialog() {
         <div className="flex flex-col space-y-4 px-6 py-4">
           {wallets.map((wallet) => (
             <div
-              className="flex items-center justify-between"
+              className="flex items-center justify-between rounded-xl border-2 border-black p-4"
               key={wallet.adapter.name}
             >
-              {wallet.adapter.name}
+              <div className="flex items-center space-x-3">
+                <Image
+                  src={wallet.adapter.icon}
+                  alt="wallet"
+                  width={24}
+                  height={24}
+                  className="c-image-shadow"
+                />
+                <span>{wallet.adapter.name}</span>
+              </div>
             </div>
           ))}
         </div>
